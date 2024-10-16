ff7de796955001a6937ad88943cdbf64a11e7db4
andrew.mamonow <andrew.mamonow@yandex.ru>
2024-10-16 13:13:26 +0300

    change main

--- main.py
+++ main.py
@@ -13,15 +13,17 @@ from model import Sale
 
 
 def create_tables(engine):
+# Функция удаления и создания базы данных   
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
 def table_print(table_title:list, books:list):
-    my_table = PrettyTable()
-    my_table.field_names = table_title
+# Функция формирования таблицы для вывода   
+    Pretty_table = PrettyTable()
+    Pretty_table.field_names = table_title
     for book in books:
-        my_table.add_row(book)
-    return my_table
+        Pretty_table.add_row(book)
+    return Pretty_table
 
 if __name__ == '__main__':
 # Программа
@@ -51,7 +53,7 @@ if __name__ == '__main__':
     table_title = ["id", "Автор"]
     print(f'Издатели в базе данных:')
     print(table_print(table_title, avtors))
-    
+
     avtor = input('Введите id издателя.')
     if avtor !='' and [tup for tup in avtors if int(avtor) in tup]:
         query = session.query(Publisher.id, Book.title, Shop.name, Sale.price, Sale.date_sale) \
